ec35d7e06a3efbecaec319132ce48f4ba43aaf9e
Samuelsndss <69823253+Samuelsndss@users.noreply.github.com>
2020-09-09 18:28:03 -0300

    commit

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,15 +1,34 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+import transactionRouter from '../routes/transaction.routes';
+
+interface Request{
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 class CreateTransactionService {
+  
+
   private transactionsRepository: TransactionsRepository;
 
   constructor(transactionsRepository: TransactionsRepository) {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({title, value, type}: Request): Transaction {
+    const {total} = this.transactionsRepository.getBalance();
+
+    if(type == "outcome" && total < value) {
+      throw new Error('O valor de caixa é inferir ao total do saque');
+    }
+    const transaction = this.transactionsRepository.create({
+      title, 
+      value, 
+      type});
+
+      return transaction;
   }
 }
 
